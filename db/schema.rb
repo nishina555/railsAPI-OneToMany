68b7ad79f0b2848391aa7864425aa80d2883ed1a
nishina <n.toshi.1.014@gmail.com>
2016-11-16 20:30:18 +0900

    Create comment model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114233944) do
+ActiveRecord::Schema.define(version: 20161116112500) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "event_id"
+    t.string   "name"
+    t.text     "profile"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_comments_on_event_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
